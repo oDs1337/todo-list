aeca35504c40289353e2c56c6191d4d22e8d8c9b
oDs1337 <odstobiaszcod@gmail.com>
2022-12-16 16:36:25 +0100

    add presenter login-box

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './containers/login/login.component';
+import { LoginBoxComponent } from './presenters/login-box/login-box.component';
 
 
 @NgModule({
   declarations: [
-    LoginComponent
+    LoginComponent,
+    LoginBoxComponent
   ],
   imports: [
     CommonModule,
---
+++ src/app/login/presenters/login-box/login-box.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-box',
+  templateUrl: './login-box.component.html',
+  styleUrls: ['./login-box.component.css']
+})
+export class LoginBoxComponent {
+
+}
