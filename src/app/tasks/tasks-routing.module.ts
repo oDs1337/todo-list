ea215f8864f77d2373cf4a150619add6fb999df8
oDs1337 <odstobiaszcod@gmail.com>
2022-12-17 22:16:07 +0100

    add routing inside tasks module and in app routing

--- src/app/tasks/tasks-routing.module.ts
+++ src/app/tasks/tasks-routing.module.ts
@@ -1,7 +1,10 @@
+import { TasksBoxComponent } from './presenters/tasks-box/tasks-box.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: TasksBoxComponent,}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
