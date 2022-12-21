3c2ac2d5868f324dfa3e3ebf325bcf4cca997808
oDs1337 <odstobiaszcod@gmail.com>
2022-12-21 19:14:49 +0100

    add working fetching data into task box container

--- src/app/tasks/presenters/tasks-box/tasks-box.component.ts
+++ src/app/tasks/presenters/tasks-box/tasks-box.component.ts
@@ -1,3 +1,4 @@
+import { Task } from './../../../interfaces/task';
 import { Component, Input } from '@angular/core';
 
 @Component({
@@ -7,6 +8,8 @@ import { Component, Input } from '@angular/core';
 })
 export class TasksBoxComponent {
 
-  @Input() done:boolean = false;
+  @Input() done: boolean = false;
+
+  @Input() tasks: Task[] = [];
 
 }
