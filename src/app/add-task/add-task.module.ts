adbd32d6400b3fb1f5ee804fe5457430926b11c6
oDs1337 <odstobiaszcod@gmail.com>
2022-12-23 16:55:57 +0100

    add stylesheet to add new task form

--- src/app/add-task/add-task.module.ts
+++ src/app/add-task/add-task.module.ts
@@ -4,6 +4,11 @@ import { CommonModule } from '@angular/common';
 import { AddTaskRoutingModule } from './add-task-routing.module';
 import { AddTaskFormComponent } from './containers/add-task-form/add-task-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+
 
 
 @NgModule({
@@ -14,6 +19,10 @@ import { ReactiveFormsModule } from '@angular/forms';
     CommonModule,
     AddTaskRoutingModule,
     ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
   ],
   exports: [
     AddTaskFormComponent
