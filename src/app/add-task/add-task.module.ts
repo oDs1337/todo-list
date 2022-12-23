0f5a0b5bd4a6b65178773dffde0d632352e6f291
oDs1337 <odstobiaszcod@gmail.com>
2022-12-23 17:13:21 +0100

    add changes in stylesheet

--- src/app/add-task/add-task.module.ts
+++ src/app/add-task/add-task.module.ts
@@ -8,6 +8,8 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 
 
@@ -23,6 +25,8 @@ import { MatButtonModule } from '@angular/material/button';
     MatInputModule,
     MatSelectModule,
     MatButtonModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
   ],
   exports: [
     AddTaskFormComponent
