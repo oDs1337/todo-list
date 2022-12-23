f9d1887fe528065b05b71c2dd766d5c22526968e
oDs1337 <odstobiaszcod@gmail.com>
2022-12-23 17:41:44 +0100

    remove login module, component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { tasksReducer } from './state/tasks.reducer';
 import { SharedModule } from './shared/shared.module';
-import { NgModule, isDevMode } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 
