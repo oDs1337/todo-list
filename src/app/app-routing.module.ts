b1333adac5f20e6af297b5d0da54717c1df7a43b
oDs1337 <odstobiaszcod@gmail.com>
2022-12-29 13:07:46 +0100

    add better routing with id in url

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ const routes: Routes = [
   { path: '', redirectTo: 'tasks', pathMatch: 'full'},
   { path: 'tasks', loadChildren: () => import('./tasks/tasks.module').then(m => m.TasksModule) },
   { path: 'add-task', loadChildren: () => import('./add-task/add-task.module').then(m => m.AddTaskModule), },
-  { path: 'edit-task', loadChildren: () => import('./edit-task/edit-task.module').then(m => m.EditTaskModule), },
+  { path: 'edit-task/:id', loadChildren: () => import('./edit-task/edit-task.module').then(m => m.EditTaskModule), },
 ];
 
 @NgModule({
