af2e84b05b227e43d3a22c0f0bcb96ae76be2deb
oDs1337 <odstobiaszcod@gmail.com>
2023-01-04 01:34:56 +0100

    add remote database in mongodb atlas

--- src/app/shared/services/tasks-database.service.ts
+++ src/app/shared/services/tasks-database.service.ts
@@ -9,8 +9,8 @@ import { fetch } from 'src/app/shared/state/tasks.action';
 })
 export class TasksDatabaseService {
 
-  #databaseUrl = "http://[::1]:3000/tasks?filter=%7B%0A%20%20%22offset%22%3A%200%2C%0A%20%20%22limit%22%3A%20100%2C%0A%20%20%22skip%22%3A%200%2C%0A%20%20%22order%22%3A%20%22string%22%2C%0A%20%20%22where%22%3A%20%7B%0A%20%20%20%20%22additionalProp1%22%3A%20%7B%7D%0A%20%20%7D%2C%0A%20%20%22fields%22%3A%20%7B%0A%20%20%20%20%22id%22%3A%20true%2C%0A%20%20%20%20%22creationDate%22%3A%20true%2C%0A%20%20%20%20%22expiryDate%22%3A%20true%2C%0A%20%20%20%20%22taskDescription%22%3A%20true%2C%0A%20%20%20%20%22isDone%22%3A%20true%2C%0A%20%20%20%20%22isRemoved%22%3A%20true%2C%0A%20%20%20%20%22removedDate%22%3A%20true%0A%20%20%7D%0A%7D";
-  #tasksUrl = "http://[::1]:3000/tasks/";
+  #databaseUrl = "http://ods1337.pl:3000/tasks?filter=%7B%0A%20%20%22offset%22%3A%200%2C%0A%20%20%22limit%22%3A%20100%2C%0A%20%20%22skip%22%3A%200%2C%0A%20%20%22order%22%3A%20%22string%22%2C%0A%20%20%22where%22%3A%20%7B%0A%20%20%20%20%22additionalProp1%22%3A%20%7B%7D%0A%20%20%7D%2C%0A%20%20%22fields%22%3A%20%7B%0A%20%20%20%20%22id%22%3A%20true%2C%0A%20%20%20%20%22creationDate%22%3A%20true%2C%0A%20%20%20%20%22expiryDate%22%3A%20true%2C%0A%20%20%20%20%22taskDescription%22%3A%20true%2C%0A%20%20%20%20%22isDone%22%3A%20true%2C%0A%20%20%20%20%22isRemoved%22%3A%20true%2C%0A%20%20%20%20%22removedDate%22%3A%20true%0A%20%20%7D%0A%7D";
+  #tasksUrl = "http://ods1337.pl:3000/tasks/";
 
   constructor(private http: HttpClient, private store: Store) { }
 
